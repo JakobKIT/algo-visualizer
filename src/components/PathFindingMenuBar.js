d7a81308e511631e3c133affd42e386021d2c55f
Jakob Klamser <mail@jakobklamser.net>
2020-03-31 13:23:14 +0200

    styling

--- src/components/PathFindingMenuBar.js
+++ src/components/PathFindingMenuBar.js
@@ -9,16 +9,18 @@ export default function PathFindingMenuBar({
   isDrawWall,
   startDijkstra
 }) {
-  const startCss = isStartClicked ? 'btn btn-secondary btn-active' : 'btn btn-secondary';
-  const finishCss = isFinishClicked ? 'btn btn-secondary btn-active' : 'btn btn-secondary';
-  const drawCss = isDrawWall ? 'btn btn-secondary btn-active' : 'btn btn-secondary';
+  const startCss = isStartClicked ? 'ml-2 btn btn-primary btn-active' : 'ml-2 btn btn-primary';
+  const finishCss = isFinishClicked ? 'ml-2 btn btn-primary btn-active' : 'ml-2 btn btn-primary';
+  const drawCss = isDrawWall ? 'ml-2 btn btn-primary btn-active' : 'ml-2 btn btn-primary';
 
   return (
-    <div className="btn-group menu-bar" role="group">
-      <button onClick={selectStartNode} type="button" className={`${startCss}`}>Select Start</button>
-      <button onClick={selectFinishNode} type="button" className={`${finishCss}`}>Select End</button>
-      <button onClick={toggleWallDraw} type="button" className={`${drawCss}`}>Draw Wall</button>
-      <button onClick={startDijkstra} type="button" className="btn btn-secondary">Start Dijkstra</button>
+    <div className="container">
+      <div className="btn-group menu-bar" role="group">
+        <button onClick={selectStartNode} type="button" className={`${startCss}`}>Select Start</button>
+        <button onClick={selectFinishNode} type="button" className={`${finishCss}`}>Select End</button>
+        <button onClick={toggleWallDraw} type="button" className={`${drawCss}`}>Draw Wall</button>
+        <button onClick={startDijkstra} type="button" className="ml-2 btn btn-primary">Start Dijkstra</button>
+      </div>
     </div>
   );
 }
